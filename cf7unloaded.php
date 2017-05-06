38256be520c4d3da2e1381a27ca4419d1d7a8cc9
Damien Carbery <damiencarbery@users.noreply.github.com>
2017-05-06 22:49:23 +0100

    Update cf7unloaded.php (#1)
    Change the remove_action() parameter as wpcf7_enqueue_scripts() and wpcf7_enqueue_styles() are no longer called by do_action().

--- cf7unloaded.php
+++ cf7unloaded.php
@@ -15,8 +15,8 @@ Version: 1.0.0
 function cf7unloaded_deregister_contact_form() {
 	global $post;
     if ( ! has_shortcode( $post->post_content, 'contact-form-7' ) ) {
-        remove_action('wp_enqueue_scripts', 'wpcf7_enqueue_styles');
-        remove_action('wp_enqueue_scripts', 'wpcf7_enqueue_scripts');
+        remove_action('wp_enqueue_scripts', 'wpcf7_do_enqueue_scripts');
     }
 }
 add_action( 'wp', 'cf7unloaded_deregister_contact_form');
+// or: add_action( 'wp_enqueue_scripts', 'cf7unloaded_deregister_contact_form', 2);
